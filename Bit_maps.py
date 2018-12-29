c10984ac4dbafb20cfc3059d367c7bea4903a7ea
wuxikun <xikun.wu@ushow.media>
2018-12-29 16:53:59 +0800

    bitmap

--- Bit_maps.py
+++ Bit_maps.py
@@ -15,10 +15,9 @@ class XC_bitmap(object):
         if num > self.capacity:
             print("你超纲了啊\n")
             return
+        self.update_value(target_value=num, flag=1)
 
-        index, loc = self.find_location(num)
 
-        self.values[index] = self.values[index] | (1<<loc)
 
     def div_num(self, num):
         n = 0
@@ -29,49 +28,58 @@ class XC_bitmap(object):
     def remove(self, num):
         if num > self.capacity:
             print("你超纲了啊\n")
+        self.update_value(target_value=num, flag=0)
 
     def find_location(self, target_value):
         # 1. 找到在第几个整数上
         index_of_bit = (target_value >> self.div_num)
         # 2. 在字节上的位置
-        loc_in_bit = target_value % self.div_num
+        loc_in_bit = target_value % self.max_bits_per_int
         return index_of_bit, loc_in_bit
 
     def query_value(self, target_value):
         index, loc = self.find_location(target_value)
-        tmp = sys.maxsize  # 全1的整数
-        tmp_1_others_0 = 1<< ((loc-1) if loc>=1 else 0)
+        tmp_1_others_0 = 1<< loc
+        # values_bin = bin(self.values[index])
+        # tmp_1_others_0_str = bin(tmp_1_others_0)
         if self.values[index] & tmp_1_others_0 == 0:
             return False
         else:
             return True
 
     def update_value(self, target_value, flag=1):
-        # 1. 找到在第几个整数上
-        index_of_bit = (target_value >> self.div_num)
-        # 2. 在字节上的位置
-        loc_in_bit = target_value % self.div_num
-        if flag == 1:
-            pass
+
+        if flag == 1 and not self.query_value(target_value):
+            index, loc = self.find_location(target_value)
+            self.values[index] = self.values[index] | (1 << loc)
+        elif flag == 0 and self.query_value(target_value):
+            index, loc = self.find_location(target_value)
+            self.values[index] = self.values[index] ^ (1 << loc)
         else:
             pass
 
+    def print(self, ascent=True):
+        pass
 
 if __name__ == "__main__":
     bit_map = XC_bitmap(1000)
     print(" need bit memory_size {a} 个整数(s) \n".format(a=bit_map.bit_size))
     num_list = [0, 1, 2, 3, 6, 8, 19, 31, 32,36, 63, 64, 65, 125, 126, 126, 128, 129, 98, 999]
     for _ in num_list:
-        print(str(_) + " location " + str(bit_map.find_location(_)))
-
         bit_map.add(_)
 
-    # for v in bit_map.values:
-    #     print(bit_map.find_location(v))
-    #     print(bin(v))
-    #
-    # print("\n test _data \n")
-    #
-    # test_num_list = [1, 2, 3, 6, 8, 19, 31, 32,36, 63, 64, 65, 125, 126, 126, 128, 129, 98, 999]
-    # for _ in test_num_list:
-    #     print(str(_) + " res " + str(bit_map.query_value(_)))
+    print("\n test _data \n")
+    test_num_list = [1, 2, 3, 6, 8, 19, 31, 32,36, 63, 64, 65, 125, 126, 126, 128, 129, 98, 999]
+    for _ in test_num_list:
+        print(str(_) + " is_in " + str(bit_map.query_value(_)))
+
+    sub_list= [6, 8, 19, 31, 32,36]
+    for _ in sub_list:
+        bit_map.remove(_)
+
+    for _ in test_num_list:
+        print(str(_) + " is_in " + str(bit_map.query_value(_)))
+
+
+
+
