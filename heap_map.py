51dc538e4da7fc19d72964a6d302c876f07b27b5
wuxikun <linuxjade@163.com>
2019-02-14 17:55:26 +0800

    heap_sort

---
+++ heap_map.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+#coding:utf-8
+
+
+def HeapSort(A):
+    def heapify(A):
+        start = (len(A) - 2) // 2
+        while start >= 0:
+            siftDown(A, start, len(A) - 1)
+            start -= 1
+
+    def siftDown(A, start, end):
+        root = start
+
+        while root * 2 + 1 <= end:
+            child = root * 2 + 1
+            print('\n A:', A, ' length', len(A))
+            print('root: ', root, ' child:', child, ' end: ', end, ' [root]:', A[root], ' [child]:', A[child],  ' [end]: ', A[end])
+
+            if child + 1 <= end and A[child] < A[child + 1]:
+                # 遍历子节点
+                child += 1
+                print('child += 1', ' [child]: ', A[child])
+            if child <= end and A[root] < A[child]:
+                # 交换
+                A[root], A[child] = A[child], A[root]
+                print('exchange')
+                root = child
+            else:
+                return
+
+    heapify(A)
+    print('------------ ------------')
+    end = len(A) - 1
+    while end > 0:
+        A[end], A[0] = A[0], A[end]
+        siftDown(A, 0, end - 1)
+        end -= 1
+
+if __name__ == '__main__':
+
+    T = [13, 14, 94, 33, 82, 25, 59, 94, 65, 23, 45, 27, 73, 25, 39, 10]
+
+    HeapSort(T)
+    print(T)
